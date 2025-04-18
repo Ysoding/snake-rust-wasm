223944d4a4ce608c9f7e92e9ec22250aadedce7f
Xmchx <1394466835@qq.com>
2025-04-18 10:56:14 +0800

    snake render

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -3,4 +3,6 @@ pub use web::*;
 
 pub trait PlatformRenderer {
     fn fill_rect(&self, x: i32, y: i32, w: i32, h: i32, color: u32);
+    fn stroke_rect(&self, x: i32, y: i32, w: i32, h: i32, color: u32);
+    fn fill_text(&self, x: i32, y: i32, text: &str, font_size: u32, color: u32);
 }
